4d9b207fe0ef1532eaaf54a22408acddfa19b9fc
Yifan <53184905+YifanLi1997@users.noreply.github.com>
2021-02-08 17:54:32 +0800

    Added level manager and finished laser movement

---
+++ Assets/Scripts/ShootTrainingLaser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootTrainingLaser : MonoBehaviour
+{
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] Transform shootingPlace;
+    [SerializeField] float shootingTimeGapInSeconds = 2f;
+    float shootingTimer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shootingTimer >= shootingTimeGapInSeconds)
+        {
+            Shoot();
+            shootingTimer = 0f;
+        }
+        else
+        {
+            shootingTimer += Time.deltaTime;
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject laser = Instantiate(laserPrefab, shootingPlace.position, Quaternion.identity) as GameObject;
+    }
+}
