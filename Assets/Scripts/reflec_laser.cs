d2fce8ae7f20ff3099083b29dc7441e45a7a82af
AlexTintin <alexis.benamira@knights.ucf.edu>
2021-02-09 20:40:23 -0500

    hit_life_reduce
    life is reduced when hit by a laser
    problem when laser reflected by sber

--- Assets/Scripts/reflec_laser.cs
+++ Assets/Scripts/reflec_laser.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using HTC.UnityPlugin.Vive;
 
 public class reflec_laser : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Lightsaber;
+    public GameObject player;
+    public Image lifebar;
 
-    // Update is called once per frame
-    void Update()
+    public GameObject tinyexplosion;
+    public SteamVR_TrackedObject sabercontrol;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
     {
-        
+        //Debug.Log("before collided");
+        if (other.gameObject == Lightsaber)
+        {
+
+            Debug.Log(other.transform.position);
+            Vector3 normal = new Vector3(other.transform.position.x, -Lightsaber.transform.position.y+ other.transform.position.y,-Lightsaber.transform.position.z + other.transform.position.z);
+            this.gameObject.transform.forward = this.gameObject.transform.forward - 2 * normal * Vector3.Dot(normal, this.gameObject.transform.forward);
+            GameObject.Destroy(GameObject.Instantiate(tinyexplosion, other.transform.position, Quaternion.identity), 0.1f);
+            SteamVR_Controller.Input((int)sabercontrol.index).TriggerHapticPulse((ushort)500);
+            
+        }
+        if (other.gameObject == player)
+        {
+            Debug.Log("alright");
+            lifebar.rectTransform.offsetMax -= new Vector2(10, 0);
+
+            //play music
+
+            GameObject.Destroy(this.gameObject);
+
+
+
+        }
     }
 }
